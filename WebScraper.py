e43eb370b3092142a77c47ac793f30b87f5475c6
mekhaelt <106632379+mekhaelt@users.noreply.github.com>
2022-12-03 15:54:21 -0500

    Updated the code; fixed up most of the excel functionality

--- WebScraper.py
+++ WebScraper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from datetime import datetime
 import pandas as pd
+import random
   
 
 
@@ -22,8 +23,9 @@ while(True):
     current_time = now.strftime("%H:%M:%S")
     print(f'At time : {current_time} IST')
     stocks=['none']*100
-    columns=[1]*100
+    percents=[1]*100
     c=0
+
 
     for x in range(1, 101):
         curr_path = ''
@@ -33,22 +35,28 @@ while(True):
         try:
             curr_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[1]/span/sup'
             title = driver.find_element("xpath", curr_path)
+
+            other_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[2]/span'
+            percent = driver.find_element("xpath", other_path)
+
         except:
             continue
         stocks[c]=(title.text)
+        percents[c]=(percent.text)
+
        
         c += 1
-        columns[c-1]=c
-    # print(stocks)
-    # chart1 = pd.Series(stocks)
+  
+    #stocks[0]=random.randint(0,9)
+    df = pd.DataFrame()
+    df['Biggest Gainers'] = stocks
+    df['% Change'] = percents
 
-    df = pd.DataFrame(stocks, index=columns)
-    #df.to_excel('df.xlsx')
+    df.to_excel('df.xlsx', index=False)
 
-    #print(chart1)
+
+
 
     # to stop the running of code for 10 mins
-    time.sleep(600) 
-  
+    time.sleep(20) 
 
-    
