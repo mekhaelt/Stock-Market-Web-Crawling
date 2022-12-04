d4bce4f8fed5856c109e0320462093c734259ab4
mekhaelt <106632379+mekhaelt@users.noreply.github.com>
2022-12-03 20:14:17 -0500

    Added in comments to make the code more comprehendable

--- WebScraper.py
+++ WebScraper.py
@@ -3,35 +3,36 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from datetime import datetime
 import pandas as pd
-import random
   
 
-
-s = Service("D:\chromedriver")
-  
+# creating the webdriver to open the browser
+s = Service("D:\chromedriver") 
 driver = webdriver.Chrome(service=s)
   
+# opening the browser
 url = 'https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/'
-  
 driver.get(url)
-  
+
+
 while(True):
     now = datetime.now()
       
-    # this is just to get the time at the time of 
-    # web scraping
+    # gets the time of the web scraping
     current_time = now.strftime("%H:%M:%S")
     print(f'At time : {current_time} IST')
+    
+    # arrays to store collected data
     stocks=['none']*100
     percents=[1]*100
+    
+    # counter variable
     c=0
 
 
     for x in range(1, 101):
         curr_path = ''
           
-        # Exception handling to handle unexpected changes
-        # in the structure of the website
+        # collecting information from the website
         try:
             curr_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[1]/span/sup'
             title = driver.find_element("xpath", curr_path)
@@ -43,20 +44,17 @@ while(True):
             continue
         stocks[c]=(title.text)
         percents[c]=(percent.text)
-
-       
         c += 1
   
-    #stocks[0]=random.randint(0,9)
+    # creating chart and importing into excel
     df = pd.DataFrame()
     df['Biggest Gainers'] = stocks
     df['% Change'] = percents
-
     df.to_excel('df.xlsx', index=False)
 
 
 
 
-    # to stop the running of code for 10 mins
+    # stops and reruns the code every 10 minutes
     time.sleep(20) 
 
