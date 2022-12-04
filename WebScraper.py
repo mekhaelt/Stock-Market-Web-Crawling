4462ff34d79a5a72e7ab97b92358c962931ea1f7
mekhaelt <106632379+mekhaelt@users.noreply.github.com>
2022-12-03 21:15:16 -0500

    Retrieves more information; when trying to retrieve the prices of the stocks, all the arrays get set to their original values

--- WebScraper.py
+++ WebScraper.py
@@ -24,37 +24,48 @@ while(True):
     # arrays to store collected data
     stocks=['none']*100
     percents=[1]*100
+    prices=[1]*100
+    volumes=[1]*100
     
     # counter variable
     c=0
 
 
     for x in range(1, 101):
-        curr_path = ''
           
         # collecting information from the website
         try:
-            curr_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[1]/span/sup'
-            title = driver.find_element("xpath", curr_path)
+            stock_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[1]/span/sup'
+            title = driver.find_element("xpath", stock_path)
 
-            other_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[2]/span'
-            percent = driver.find_element("xpath", other_path)
+            change_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[2]/span'
+            percent = driver.find_element("xpath", change_path)
+
+            cost_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[3]/text()'
+            #cost = driver.find_element("xpath", cost_path)
+
+            volume_path = f'/html/body/div[3]/div[4]/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div/div/table/tbody/tr[{x}]/td[6]'
+            vol = driver.find_element("xpath", volume_path)
+
+
+
 
         except:
             continue
         stocks[c]=(title.text)
         percents[c]=(percent.text)
+        volumes[c]=(vol.text)
         c += 1
   
     # creating chart and importing into excel
     df = pd.DataFrame()
     df['Biggest Gainers'] = stocks
     df['% Change'] = percents
-    df.to_excel('df.xlsx', index=False)
+    df['Volume'] = volumes
+    df.to_excel('Stock Info.xlsx', index=False)
 
 
 
 
     # stops and reruns the code every 10 minutes
     time.sleep(20) 
-
